ac7fcf9ffc2633dfba2b5269da96ce6a7852abd7
Decagon <decagon@Decagons-MacBook-Air.local>
2022-10-15 07:06:52 +0100

    setup the register route and added express.json and urlencoded for parsing data into the requests

--- index.ts
+++ index.ts
@@ -1,13 +1,25 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, urlencoded } from 'express';
 import dotenv from 'dotenv';
+import { register } from './interfaces/interface.register';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const BASE_URL: string = "/auth/v1";
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.post(`${BASE_URL}/register`, (req: Request, res: Response) => {
+   let data:register = req.body
+   
+   res.status(201).json({
+    message:"User Resgistered Successfully"
+   })
 });
 
 app.listen(port, () => {
